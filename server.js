1027bbbdeff0f81b2084ccf851839a17ae8ff4f6
znDev0101 <zulfanurhuda01@gmail.com>
2024-10-18 09:39:10 +0700

    cahnge config cors

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.use(express.json());
 app.use(
   cors({
     origin: "https://test-frontend-tahap1.vercel.app",
+    methods: "GET, POST, PUT, DELETE",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 
