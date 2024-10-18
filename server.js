a48187f7c5aff54470848d199f87fe38a5e053a0
znDev0101 <zulfanurhuda01@gmail.com>
2024-10-18 14:36:41 +0700

    fix cors

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://test-frontend-tahap1.vercel.app/login",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
