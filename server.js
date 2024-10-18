c08ef557772196963b5edd6e7efd246f6b37bef7
znDev0101 <zulfanurhuda01@gmail.com>
2024-10-18 09:56:45 +0700

    change config

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: "https://test-frontend-tahap1.vercel.app",
-    methods: "GET, POST, PUT, DELETE",
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
