1f50b64e5737ed91fca1ccce5c013a3324a198b9
znDev0101 <zulfanurhuda01@gmail.com>
2024-10-18 14:17:14 +0700

    fix cors error

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use(
   cors({
     origin: "https://test-frontend-tahap1.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: "Content-Type,Authorization",
     credentials: true,
   })
 );
